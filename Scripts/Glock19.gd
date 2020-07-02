e3f8b0d2d958609a56cc64e37866dd6b3c2b069a
llister15 <llister@wonkasoft.com>
2020-07-02 12:52:09 -0700

    added sound

--- Scripts/Glock19.gd
+++ Scripts/Glock19.gd
@@ -37,6 +37,7 @@ func Shoot():
 #		$"Muzzle Position".position = $"Muzzle Position".position
 	if shoot_gun and is_reloading == false:
 		if fire_rate <= 0 and mag_size > 0:
+			$"9mm_shot".play()
 			fire_rate = FIRE_RATE_MAX
 			mag_size -= 1
 			get_tree().get_root().add_child(bullet_instance)
@@ -44,17 +45,21 @@ func Shoot():
 			bullet_instance.rotation = $"Muzzle Position".global_rotation 
 			bullet_instance.apply_impulse(Vector2(), Vector2(mouseTarget.normalized().x * bulletSpeed, mouseTarget.normalized().y * bulletSpeed))
 	
-	
 
 func Reload():
 	var reload = Input.is_action_just_pressed("Reload")
 	if reload and mag_size < MAG_SIZE_MAX:
 		is_reloading = true
+		$"dry_fire".play()
+		$"ejecting_magazine".play()
+		$"pop_clip_in".play()
 		yield(get_tree().create_timer(RELOAD_TIME_MAX), "timeout")
 		mag_size = MAG_SIZE_MAX
 		is_reloading = false
 	if mag_size <= 0:
 		is_reloading = true
+		$"ejecting_magazine".play()
+		$"pop_clip_in".play()
 		yield(get_tree().create_timer(RELOAD_TIME_MAX), "timeout")
 		mag_size = MAG_SIZE_MAX
 		is_reloading = false
